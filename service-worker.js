067bdeea1cd2b4bce18f2670b24ae68694810e0f
Fatou <fatou42diallo@gmail.com>
2026-09-17 00:03:09 +0200

    3notes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "g_notes-v2"; // ← incrémente ce numéro à chaque déploiement important
+const CACHE_NAME = "g_notes-v3"; // ← incrémente ce numéro à chaque déploiement important
 const FICHIERS_A_METTRE_EN_CACHE = [
   "./",
   "./index.html",
